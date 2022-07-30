d785d7d820743a60fd3b581c2f5001addf9ea5b3
seoyun <may-primavera@naver.com>
2022-07-30 22:18:37 +0900

    Design: select타입 선택시 input 박스 생성
    main 페이지

--- src/main/resources/static/bootstrap/js/lottoScript.js
+++ src/main/resources/static/bootstrap/js/lottoScript.js
@@ -2,24 +2,41 @@
  * 변수선언
  */
 
+let selectVal;
+
 //로딩시 지난주 당첨번호 불러오기(저장 필요 없음)
 $(document).ready(function(){
     console.log('로딩 성공');
-    getLottoNum();
 });
 
-function getLottoNum(){
-    console.log('getLottoNum 성공');
+function showInputBox() {
+    console.log('typeSelect 성공');
+
+    selectVal = $('#selectType').val();
+    if (selectVal == 'selectNum' || selectVal == 'allSelect') {
+        console.log('selectNum, allSelect');
+        render(selectVal);
+    } else {
+        removeRender(selectVal);
+    }
+
 }
 
-$('#selectNum').on('click', function (){
-    console.log('반자동 클릭');
+function render(selectVal) {
+    console.log('inputBox 생성');
+    $('#showInputBox')
+        .html('<input type="text" name="inputNum" value="" placeholder="\',\'를 포함한 1~45 사이의 번호를 입력해주세요">');
 
-})
+    selectVal.remove();
+}
 
-$('#allSelect').on('click', function (){
-    console.log('전체수동 클릭');
+function removeRender(selectVal) {
+    $('#showInputBox')
+        .html('');
 
-})
+    selectVal.remove();
+}
+
+
 
 
