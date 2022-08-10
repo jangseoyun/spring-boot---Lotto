cd428cdea47b537ba1b2fc531615e8eb24589033
seoyun <may-primavera@naver.com>
2022-08-10 21:16:49 +0900

    feat: 순위를 묶어서 관리하는 RankCommand와 factory 생성

--- src/main/java/lotto/practice/random/domain/winning/WinningFactory.java
+++ src/main/java/lotto/practice/random/domain/winning/WinningFactory.java
@@ -1,7 +1,7 @@
 package lotto.practice.random.domain.winning;
 
 import lombok.extern.slf4j.Slf4j;
-import lotto.practice.random.domain.winning.dto.WinningDto;
+import lotto.practice.random.domain.winning.command.WinningResult;
 import lotto.practice.random.domain.winning.entity.WinningInfo;
 
 import java.util.List;
@@ -10,29 +10,30 @@ import java.util.Map;
 @Slf4j
 public class WinningFactory {
 
-    public static WinningInfo createWinningInfo(WinningDto winner, Map<String, Long> winningAmount, List<Integer> totalCount) {
+    public static WinningInfo createWinningInfo(WinningResult winner, Map<RankType, Long> winningAmount, List<Integer> totalCount) {
         //몇등인지 알아야함
         return WinningInfo.builder()
                 .totSellingPrice(winner.getTotSellingPrice())
                 .user(winner.getUser())
                 .lottoCycleNum(winner.getLottoCycleNum())
                 .winnerRank(winner.getWinnerRank())
-                .totalAmount(getAmount(winningAmount, winner.getWinnerRank()))
-                .winnerTotalCount(getTotalCount(totalCount, winner.getWinnerRank()))
+                //.totalAmount(getAmount(winningAmount, winner.getWinnerRank()))
+                //.winnerTotalCount(getTotalCount(totalCount, winner.getWinnerRank()))
                 .build();
     }
 
-    public static Long getAmount(Map<String, Long> winningAmount, int rank) {
+    //TODO: getAmount, getTotalCount Rank 객체로 뺄 것
+    public static Long getAmount(Map<RankType, Long> winningAmount, int rank) {
         switch (rank) {
             case 5:
-                return winningAmount.get("fifthTotalAmount");
+                return winningAmount.get(RankType.FIFTH);
             case 4:
-                return winningAmount.get("fourthTotalAmount");
+                return winningAmount.get(RankType.FOURTH);
             case 3:
             case 2:
-                return winningAmount.get("secondAndThirdAmount");
+                return winningAmount.get(RankType.SECOND);
             case 1:
-                return winningAmount.get("firstAmount");
+                return winningAmount.get(RankType.FIRST);
             default:
                 break;
         }
