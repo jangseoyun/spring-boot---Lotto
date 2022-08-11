4f97007bfcd77fd9d00400149c71de3c6d1b379c
seoyun <may-primavera@naver.com>
2022-08-11 18:46:15 +0900

    feat: 회차 요청시 화면에서 받을 당첨 정보 로직
    당회차 등위별 당첨자 수와 당첨금을 한 객체로 만들었음

--- src/main/java/lotto/practice/random/domain/winningAmount/WinningAmountService.java
+++ src/main/java/lotto/practice/random/domain/winningAmount/WinningAmountService.java
@@ -3,6 +3,7 @@ package lotto.practice.random.domain.winningAmount;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lotto.practice.random.domain.winningAmount.rank.TotalCount;
+import lotto.practice.random.domain.winningInfo.WinningMap;
 import lotto.practice.random.infrastructure.WinningDbRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,13 +20,15 @@ public class WinningAmountService {
     /**
      * 당회차 등위별 당첨금 계산
      */
-    public RankAmountCommand calcAmount(Long lottoCycleNum) {
+    public WinningMap calcAmount(Long lottoCycleNum) {
         TotalCount rankTotalCount = calculatAmount.getRankTotalCount(lottoCycleNum);//당회차 등위별 당첨자 수
         Long totalSellAmount = winningDbRepository.getTotalSellAmount(lottoCycleNum);//당회차 전체 판매 금액
 
         //등위별 금액 계산
         RankAmountCommand getRankAmount = calculatAmount.getWinningAmount(rankTotalCount, totalSellAmount);
-        return getRankAmount;
+
+        WinningMap winningMap = new WinningMap(getRankAmount, rankTotalCount);
+        return winningMap;
     }
 
 
---
+++ src/main/java/lotto/practice/random/domain/winningInfo/WinningService.java
@@ -0,0 +1,45 @@
+package lotto.practice.random.domain.winningInfo;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lotto.practice.random.domain.Winner.entity.Winner;
+import lotto.practice.random.infrastructure.WinnerDbRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class WinningService {
+
+    private final WinnerDbRepository winnerDbRepository;
+
+    /**
+     * 회차 전체 리스트 가져오기
+     */
+    public List<Long> getCycleNumList() {
+        List<Long> cycleNumList = winnerDbRepository.getCycleNumList();
+        log.info("회차 전체 리스트 : " + cycleNumList);
+        return cycleNumList;
+    }
+
+    /**
+     * 화면에 보낼 winningInfo 만들기
+     */
+    public List<WinningInfoCommand> makeWinningInfo(WinningMap winningMap) {
+        List<WinningInfoCommand> resultWinInfo = new ArrayList<>();
+
+        //winnerList 가지고 오기 winner + 등수에 따라 RankAmountCommand 필요
+        List<Winner> allWinner = winnerDbRepository.findAllWinner();
+        for (Winner winner : allWinner) {
+            WinningInfoCommand createWinningInfo = WinningInfoFactory.setWinningInfo(winner, winningMap);
+            resultWinInfo.add(createWinningInfo);
+        }
+        return resultWinInfo;
+    }
+
+}
