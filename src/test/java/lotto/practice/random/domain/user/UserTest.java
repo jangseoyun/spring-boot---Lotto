55691aa5daf204ac790ea95bcb0634615d860303
seoyun <may-primavera@naver.com>
2022-08-17 22:40:13 +0900

    test: 회원가입 테스트 코드 작성

---
+++ src/test/java/lotto/practice/random/domain/user/UserTest.java
@@ -0,0 +1,41 @@
+package lotto.practice.random.domain.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class UserTest {
+
+    @DisplayName("회원가입시 아이디 글자수 제한 체크")
+    @Test
+    void 아이디글자수제한() {
+        //exception
+        assertThatThrownBy(() -> {
+            new User("tes", "pwtest", "test@gmail.com", "010-2222-3333");
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasStackTraceContaining("아이디는 4글자 이상이어야합니다");
+    }
+
+    @DisplayName("이메일 형태 검증")
+    @Test
+    void 이메일형태검증() {
+        //exception
+        assertThatThrownBy(() -> {
+            new User("test", "pwtest", "test/gmailcom", "010-2222-3333");
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasStackTraceContaining("email 형태만 입력 가능합니다.");
+    }
+
+    @DisplayName("중복 회원 확인")
+    @Test
+    void 중복회원() {
+        //h2 테이블 만들어야함
+
+
+    }
+
+
+}
